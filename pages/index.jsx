83dde90d9cf29ba3a39e283ef61b7808a4da4009
corina-constantin <corina.constantin@zoho.com>
2022-06-13 22:09:37 +0300

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Thread affair</title>
+      </Head>
+
+
+<header className="header">Hello</header>
+
+<main className="main"></main>
+
+<footer className="footer"></footer>
+
+     
+    </div>
+  )
+}
